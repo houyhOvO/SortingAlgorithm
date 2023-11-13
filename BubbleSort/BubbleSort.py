795f008775d174711e1f9c0147fd31aca262243a
houyaohui <u7754892@anu.edu.au>
2023-11-13 21:39:09 +1100

    added .py and modified .java

---
+++ BubbleSort/BubbleSort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    print("Sorting array...")
+    bubble_sort(arr)
+    print(arr)
+    
